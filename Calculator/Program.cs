4e2746169e9ffe4c43ae0d0af59ebf8ecd244f49
Davi de Oliveira <odavi278@gmail.com>
2023-12-31 18:23:08 -0300

    Criando a função soma

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            float soma = v1 + v2;
+            Console.WriteLine($"O resultado da soma é = {soma}");
+
+        }
+    }
+}
